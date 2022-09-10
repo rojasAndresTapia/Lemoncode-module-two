89093aaf1944e092e6872b7421fde43b8f97385f
Andrés Rojas <eurekacloud2019@gmail.com>
2022-09-10 18:16:41 +0200

    Add images

--- js/app.js
+++ js/app.js
@@ -1,40 +1,37 @@
 let hotel = {
-    name: "",
-    location: "",
-    img: "",
-    rating : {
-        uno: "<span class='fa fa-star'></span><span class='fa fa-star-o'></span><span class='fa fa-star-o'></span><span class='fa fa-star-o'></span><span class='fa fa-star-o'></span>",
-        dos: "<span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star-o'></span><span class='fa fa-star-o'></span><span class='fa fa-star-o'></span>",
-        tres: "<span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star-o'></span><span class='fa fa-star-o'></span>",
-        cuatro: "<span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star-o'></span>",
-        cinco: "<span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span>",
-    },
-    review: false,
+  name: '',
+  location: '',
+  img: '',
+  rating: "<span class='fa fa-star'></span>",
+  review: false,
 };
 
-hotelRating = prompt("Puntua el hotel: uno, dos, tres, cuatro o cinco");
+hotelRating = prompt('Puntua el hotel: uno, dos, tres, cuatro o cinco');
 
-let userHotelRating = document.getElementById("hotelRating");
-userHotelRating.innerHTML = hotel.rating[hotelRating];
+let userHotelRating = document.getElementById('hotelRating');
+for (let i = 0; i < parseInt(hotelRating); i++) {
+  userHotelRating.innerHTML += hotel.rating;
+  console.log('hotel', hotel.rating);
+}
 
+console.log(typeof hotelRating);
 
-hotel.review = confirm("¿Quieres que la reseña sea anónima?");
+hotel.review = confirm('¿Quieres que la reseña sea anónima?');
 
-let userHotelReview = document.getElementById("hotelReview");
-userHotelReview.checked= hotel.review;
+let userHotelReview = document.getElementById('hotelReview');
+userHotelReview.checked = hotel.review;
 
-hotel.name = prompt("Introduce el nombre del hotel");
+hotel.name = prompt('Introduce el nombre del hotel');
 
-let userHotelName = document.getElementById("hotelName");
-userHotelName.innerHTML= "Hotel " + hotel.name;
+let userHotelName = document.getElementById('hotelName');
+userHotelName.innerHTML = 'Hotel ' + hotel.name;
 
-hotel.location = prompt("introduce la ubicación del hotel");
+hotel.location = prompt('introduce la ubicación del hotel');
 
-let userHotelLocation = document.getElementById("hotelLocation");
-userHotelLocation.innerHTML= hotel.location;
+let userHotelLocation = document.getElementById('hotelLocation');
+userHotelLocation.innerHTML = hotel.location;
 
-hotel.img = prompt("Introduce la url de la foto del hotel");
+hotel.img = prompt('Introduce la url de la foto del hotel');
 
-let userHotelImg = document.getElementById("hotelImg");
-userHotelImg.src= hotel.img;
-
+let userHotelImg = document.getElementById('hotelImg');
+userHotelImg.src = hotel.img;
